d4db348590891166b746546676fccb054ee4f749
unknown <lawrencekulet@gmail.com>
2025-03-12 17:13:00 +0800

    program 8 batch 1

--- b1-prog8-odd number counter.py
+++ b1-prog8-odd number counter.py
@@ -5,3 +5,8 @@
 # count the number of odd numbers
 # display the count of odd numbers
 
+oddnum = 0
+for numbers in range(10):
+    if float(input("Enter a number: ")) % 2:
+        oddnum += 1
+print(oddnum)
