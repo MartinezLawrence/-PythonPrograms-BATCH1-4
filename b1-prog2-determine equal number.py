306fd4a04b53a13f5dd2ddd5f2fa7abd983a5366
unknown <lawrencekulet@gmail.com>
2025-03-12 15:51:11 +0800

    OOP - BATCH1-4

---
+++ b1-prog2-determine equal number.py
@@ -0,0 +1,6 @@
+# determine equal number
+# Lawrence Andrew B. Martinez
+# This program will determine if the two numbers are equal
+# ask the user to input two numbers
+# compare the two numbers
+# display "Equal" if the two numbers are equal
