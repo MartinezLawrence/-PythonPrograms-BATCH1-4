d7341a7fb880f064841fbb4c103c22eaafee0498
unknown <lawrencekulet@gmail.com>
2025-03-12 15:57:01 +0800

    prog2-b1

--- b1-prog2-determine equal number.py
+++ b1-prog2-determine equal number.py
@@ -4,3 +4,8 @@
 # ask the user to input two numbers
 # compare the two numbers
 # display "Equal" if the two numbers are equal
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if num1 == num2:
+    print("Equal")
