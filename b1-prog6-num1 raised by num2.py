685a3b815c3c1c6c7c32f35076010f43e82f8e1a
unknown <lawrencekulet@gmail.com>
2025-03-12 16:25:49 +0800

    program 6 batch 1

---
+++ b1-prog6-num1 raised by num2.py
@@ -0,0 +1,10 @@
+# number raised by a number
+# Lawrence Andrew B. Martinez
+# This program will raise a number by a number that thas been inputted by the user
+# Ask the user the first number and the second number
+# Then raise the first number by the second number
+# Print the result
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print(num1 ** num2)
