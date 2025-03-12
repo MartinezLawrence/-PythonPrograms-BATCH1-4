4c24d5dad02abb4c548c2735b4b842d26a3305ea
unknown <lawrencekulet@gmail.com>
2025-03-12 16:12:00 +0800

    b1-prog4-product of two number.py

--- b1-prog4-product of two number.py
+++ b1-prog4-product of two number.py
@@ -2,3 +2,7 @@
 # Lawrence Andrew B. Martinez
 # This program will ask the user to input two numbers
 # then it will display the product of the two numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(num1 * num2)
