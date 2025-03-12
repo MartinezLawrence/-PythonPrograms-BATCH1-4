165bc420ea7d8c70982eba5813fd2afd2452d312
unknown <lawrencekulet@gmail.com>
2025-03-12 16:58:09 +0800

    program 8 batch 1

---
+++ b1-prog8-odd number counter.py
@@ -0,0 +1,7 @@
+# odd number counter
+# Lawrence Andrew B. Martinez
+# This program counts the number of odd numbers in 10 numbers
+# prompt the user to input 10 numbers
+# count the number of odd numbers
+# display the count of odd numbers
+
