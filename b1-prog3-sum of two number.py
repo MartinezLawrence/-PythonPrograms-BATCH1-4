3a4a60ae9f2f2ff7d05ba08cdbbb85a9b77d96da
unknown <lawrencekulet@gmail.com>
2025-03-12 16:05:59 +0800

    program 3 batch 1

--- b1-prog3-sum of two number.py
+++ b1-prog3-sum of two number.py
@@ -2,6 +2,8 @@
 # Lawrence Andrew B. Martinez
 # This program adds two numbers
 # prompt the user to enter two numbers
-# add the two numbers
 # print the sum of the two numbers
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(num1 + num2)
