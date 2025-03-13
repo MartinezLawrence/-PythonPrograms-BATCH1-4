5026a2e9ade8cfef2c2990ac5ea192dcb76160e6
unknown <lawrencekulet@gmail.com>
2025-03-14 00:29:05 +0800

    pseudocode

---
+++ display_num_from_highest_to_lowest.py
@@ -0,0 +1,8 @@
+# display num by highest to lowest
+# this program will display numbers entered by the user in descending order
+
+# initialize list
+# use while loop to get user input
+# attempt to convert input to integer
+# break loop if input is not a number
+# sort list in descending order
