a84774bbcdd5831d4d858802a5c53246af8513a4
unknown <lawrencekulet@gmail.com>
2025-03-13 13:15:37 +0800

    prog14 pseudocode

---
+++ b2-prog14-qoutient without decimal point.py
@@ -0,0 +1,5 @@
+# qoutient without decimal point
+# this program will display the quotient of two numbers without decimal point
+
+# prompt user to input two numbers
+# divide the two numbers and convert the result to integer
