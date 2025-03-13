affe564f597d84557262032a162887ba6b7f8f8b
unknown <lawrencekulet@gmail.com>
2025-03-13 22:42:27 +0800

    prog22 progress

--- display_all_numbers_and_show_duplicate.py
+++ display_all_numbers_and_show_duplicate.py
@@ -9,6 +9,10 @@ for list_of_numbers in range(10):   # 10 numbers
     numbers.append(int(input("Enter a number: ")))  # Add the numbers to the list
 
 # Use a set to keep track of numbers we have already input
+done_input = set()
+
 # Initialize an empty list to store unique entries
+unique_entries = []
+
 # Loop through each number in the input list
 # print the number if it is not in the unique list
