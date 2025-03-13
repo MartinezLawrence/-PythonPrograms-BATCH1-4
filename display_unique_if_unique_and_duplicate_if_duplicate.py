a532e09fdd7836b841893dbd541470b5b626669f
unknown <lawrencekulet@gmail.com>
2025-03-13 22:59:47 +0800

    pseudocode

---
+++ display_unique_if_unique_and_duplicate_if_duplicate.py
@@ -0,0 +1,8 @@
+# display "unique" or "duplicate" for each input
+# this program will display "unique" if the input is unique
+# and "duplicate" if the input is duplicate
+
+# Initialize an empty list to store user input numbers
+# Loop indefinitely until the user enters something invalid 
+# attempt to convert the input to an integer
+# if the input is not an integer, break out of the loop
