d2efa4245bc39cd7f6bf4910f84aabada5ac8b98
unknown <lawrencekulet@gmail.com>
2025-03-13 21:22:53 +0800

    renamed

---
+++ determine_equal_number.py
@@ -0,0 +1,11 @@
+# determine equal number
+# Lawrence Andrew B. Martinez
+# This program will determine if the two numbers are equal
+
+# ask the user to input two numbers\
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# compare the two numbers and display the result if equal
+if num1 == num2:
+    print("Equal")
---
+++ sum_of_two_number.py
@@ -0,0 +1,9 @@
+# sum of two num
+# Lawrence Andrew B. Martinez
+# This program adds two numbers
+# prompt the user to enter two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# print the sum of the two numbers
+print(num1 + num2)
