2201c6fd7172434acdc05c947eeabe02d719caab
unknown <lawrencekulet@gmail.com>
2025-03-13 21:28:09 +0800

    renamed

---
+++ num1_raised_by_num2.py
@@ -0,0 +1,10 @@
+# number raised by a number
+# Lawrence Andrew B. Martinez
+# This program will raise a number by a number that thas been inputted by the user
+
+# Ask the user the first number and the second number
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# The program will then display the first number raised by the second number
+print(num1 ** num2)
---
+++ product_of_two_number.py
@@ -0,0 +1,10 @@
+# product of two num
+# Lawrence Andrew B. Martinez
+# This program will ask the user to input two numbers then it will display the product of the two numbers
+
+# prompt the user to enter two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# then display the product of the two numbers
+print(num1 * num2)
