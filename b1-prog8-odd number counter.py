5013010409e39a4b8fe1ad9a780fd00271c4c88c
unknown <lawrencekulet@gmail.com>
2025-03-13 12:47:29 +0800

    fixed prog8

--- b1-prog8-odd number counter.py
+++ b1-prog8-odd number counter.py
@@ -1,12 +1,12 @@
 # odd number counter
 # Lawrence Andrew B. Martinez
 # This program counts the number of odd numbers in 10 numbers
-# prompt the user to input 10 numbers
-# count the number of odd numbers
-# display the count of odd numbers
 
+# prompt the user to input 10 numbers
 oddnum = 0
 for numbers in range(10):
     if float(input("Enter a number: ")) % 2:
         oddnum += 1
+
+# display the number of odd numbers
 print(oddnum)
