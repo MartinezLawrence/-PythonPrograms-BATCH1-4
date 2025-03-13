06610b721234125482ba649209e0a243716390d5
unknown <lawrencekulet@gmail.com>
2025-03-13 12:49:06 +0800

    minorchanges prog9

--- b1-prog9-all even in 0 to 100 using for loop.py
+++ b1-prog9-all even in 0 to 100 using for loop.py
@@ -1,8 +1,8 @@
 # all even in 0 to 100 using for loop
 # This program will print out all even numbers starting from 0 to 100
+
 # the range should be in 101 
 # if the number is divisible by 2 print that number
-
 for numbers in range(101):
     if numbers % 2 == 0:
         print(numbers)
