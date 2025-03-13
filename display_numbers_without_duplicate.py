7ff1fdc4634d11abe0c3c5b24998c4cf1d0233ea
unknown <lawrencekulet@gmail.com>
2025-03-13 22:02:40 +0800

    pseudocode

---
+++ display_numbers_without_duplicate.py
@@ -0,0 +1,7 @@
+# display numbers without duplicate
+# This program is to display numbers that has no duplicate
+
+# Initialize an empty list to store user input numbers
+# Loop to ask the user for 10 numbers
+# Use list comprehension to find numbers that appear only once
+# Print the numbers without duplicates
