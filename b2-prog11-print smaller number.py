e96ad070e349f6993bf065dd2ea7a0842ae5acb5
unknown <lawrencekulet@gmail.com>
2025-03-13 12:57:25 +0800

    prog11 batch2 code

--- b2-prog11-print smaller number.py
+++ b2-prog11-print smaller number.py
@@ -2,4 +2,8 @@
 # this program will display which number is smaller
 
 # prompt user for input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
 # print the smaller number
+print(min(num1, num2))
