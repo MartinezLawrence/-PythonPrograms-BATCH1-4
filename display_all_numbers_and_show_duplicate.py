5caadc6543846a58b430aacbf2a7bb9a95e52547
unknown <lawrencekulet@gmail.com>
2025-03-13 22:39:12 +0800

    prog 22 progress

--- display_all_numbers_and_show_duplicate.py
+++ display_all_numbers_and_show_duplicate.py
@@ -2,7 +2,12 @@
 # This program will display all numbers and show duplicates only once
 
 # Initialize an empty list to store user input numbers
+numbers = []
+
 # Loop to ask the user for 10 numbers
+for list_of_numbers in range(10):   # 10 numbers
+    numbers.append(int(input("Enter a number: ")))  # Add the numbers to the list
+
 # Use a set to keep track of numbers we have already input
 # Initialize an empty list to store unique entries
 # Loop through each number in the input list
