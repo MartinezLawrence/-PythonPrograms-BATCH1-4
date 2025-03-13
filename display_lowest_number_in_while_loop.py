c174f9af6ef2f58e62a490ab0082ed406b09cc67
unknown <lawrencekulet@gmail.com>
2025-03-13 23:30:34 +0800

    prog24 progress

--- display_lowest_number_in_while_loop.py
+++ display_lowest_number_in_while_loop.py
@@ -2,8 +2,18 @@
 # this program will display the lowest number in a list of numbers
 
 # Initialize an empty list to store user input numbers
+input_num = [] 
+
 # Loop indefinitely until the user enters something invalid
-# attempt to convert the input to integer
+while True:
+    try:
+        # Get the user input
+        number = int(input("Enter a number: "))  # attempt to convert the input to integer
+
+        # Add the number to the list
+        input_num.append(number)
+    except ValueError:
+        break       # if the input is not an integer, break out of the loop
 
 # check if any numbers were entered
 # if so, display the lowest number
