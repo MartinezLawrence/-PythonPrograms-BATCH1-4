0aafec2594f8c5e9366773f09b02d7bea80ffe98
unknown <lawrencekulet@gmail.com>
2025-03-13 23:09:26 +0800

    prog23 progress

--- display_unique_if_unique_and_duplicate_if_duplicate.py
+++ display_unique_if_unique_and_duplicate_if_duplicate.py
@@ -3,6 +3,17 @@
 # and "duplicate" if the input is duplicate
 
 # Initialize an empty list to store user input numbers
+input_num = []
+
 # Loop indefinitely until the user enters something invalid 
+while True:
+    try:
+        # Get the user input
+        number = int(input("Enter a number: "))
+        # Add the number to the list
+        input_num.append(number)
+    except ValueError:
+        break
+
 # attempt to convert the input to an integer
 # if the input is not an integer, break out of the loop
