6bf288f62372150f48b6018b4ba6ae94578ed252
unknown <lawrencekulet@gmail.com>
2025-03-13 22:36:42 +0800

    pseudocode

---
+++ display_all_numbers_and_show_duplicate.py
@@ -0,0 +1,9 @@
+# display all numbers and show duplicates only once
+# This program will display all numbers and show duplicates only once
+
+# Initialize an empty list to store user input numbers
+# Loop to ask the user for 10 numbers
+# Use a set to keep track of numbers we have already input
+# Initialize an empty list to store unique entries
+# Loop through each number in the input list
+# print the number if it is not in the unique list
