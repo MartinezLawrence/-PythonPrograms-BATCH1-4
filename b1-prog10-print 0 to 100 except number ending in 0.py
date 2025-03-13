649e556051a872f4da9a74273047be739b72d8d7
unknown <lawrencekulet@gmail.com>
2025-03-13 21:32:33 +0800

    removed old files

--- b1-prog10-print 0 to 100 except number ending in 0.py
+++
@@ -1,8 +0,0 @@
-# print 0 to 100 except number ending in 0
-# This program will print numbers from 0 to 100 except numbers ending in 0
-
-# range must be 101
-# if the number is divisible by 10 do not print that number
-for numbers in range(101):
-    if numbers % 10 != 0:
-        print(numbers)  # print numbers from 0 to 100 except numbers ending in 0
