a2d1eabd53d9a8091ca9367a62d5c579f5779d49
unknown <lawrencekulet@gmail.com>
2025-03-13 13:00:59 +0800

    prog 12 batch 2 pseudocode

---
+++ b2-prog12-determine not equal number.py
@@ -0,0 +1,6 @@
+# determine not equal number
+# this program will display if the numbers are not equal
+
+# prompt user to input the first number and second number
+
+# display "Not Equal" if the numbers are not equal
