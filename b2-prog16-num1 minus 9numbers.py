9cf989a4dc66823a9fe81152626c5797b9105684
unknown <lawrencekulet@gmail.com>
2025-03-13 13:39:54 +0800

    prog 16 pseudocode

---
+++ b2-prog16-num1 minus 9numbers.py
@@ -0,0 +1,7 @@
+# num1 minus 9numbers
+# this program will print the result of the first number minus all of the remaining numbers
+
+# prompt the user to enter the first number
+# the range of the loop is 9 because we are going to ask the user to enter 9 numbers
+# prompt the user to enter the next 9 number
+# print the result of the first number minus all of the remaining numbers
