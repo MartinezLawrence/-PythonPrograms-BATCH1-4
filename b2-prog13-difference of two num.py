f2e5b8bfbd78aaff6c53b1dd49ce6d5be6ef7970
unknown <lawrencekulet@gmail.com>
2025-03-13 13:07:47 +0800

    prog 13 pseudocode

---
+++ b2-prog13-difference of two num.py
@@ -0,0 +1,5 @@
+# difference of two num
+# This program will take two numbers as input and display the difference of those two numbers
+
+# prompt the user to enter the first number and second number
+# print the difference of the two numbers
