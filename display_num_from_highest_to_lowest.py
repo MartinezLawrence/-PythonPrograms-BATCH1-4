0d661839c0b72820dea9b9eac189ef943d1ca147
unknown <lawrencekulet@gmail.com>
2025-03-14 00:31:35 +0800

    prog29 finished

--- display_num_from_highest_to_lowest.py
+++ display_num_from_highest_to_lowest.py
@@ -2,7 +2,14 @@
 # this program will display numbers entered by the user in descending order
 
 # initialize list
+number_list = []
+
 # use while loop to get user input
-# attempt to convert input to integer
-# break loop if input is not a number
+while True:
+    try:
+        number_list.append(int(input("Enter a number: ")))  # append the number to the list
+    except ValueError:
+        break     # break out the loop if the input is invalid
+
 # sort list in descending order
+print(sorted(number_list, reverse=True))
