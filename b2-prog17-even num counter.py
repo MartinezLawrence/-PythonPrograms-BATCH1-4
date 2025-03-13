d7495df66423f24fc1332767efb5a55a041ff719
unknown <lawrencekulet@gmail.com>
2025-03-13 14:18:42 +0800

    prog 17 batch 2 code

--- b2-prog17-even num counter.py
+++ b2-prog17-even num counter.py
@@ -1,6 +1,10 @@
 # even num counter
 # this program will count the number of even numbers
 
-# ask user to input 10 numbers
-# if the number is divisible by 2, add 1 to the counter
+evennum = 0  # initialize the counter
+for numbers in range(10):   # ask user to input 10 numbers
+    if float(input("Enter a number: ")) % 2 == 0:   # check if the number is divisible by 2
+        evennum += 1  # add 1 to the counter if the number is divisible by 2
+
 # print the number of even numbers
+print(evennum) 
