79784f94455bf52a4961e8dced2849f058f9dfb4
unknown <lawrencekulet@gmail.com>
2025-03-13 13:24:16 +0800

    prog 15 pseudocode

---
+++ b2-prog15-print remainder.py
@@ -0,0 +1,6 @@
+# print remainder
+# this program will print the remainder when the first number is divided by the second number
+
+# prompt user for two numbers
+
+# print the remainder when the first number is divided by the second number
