10f77044016f062ed009972c7adecd710969979a
unknown <lawrencekulet@gmail.com>
2025-03-13 13:57:29 +0800

    prog 17 pseudocode

---
+++ b2-prog17-even num counter.py
@@ -0,0 +1,6 @@
+# even num counter
+# this program will count the number of even numbers
+
+# ask user to input 10 numbers
+# if the number is divisible by 2, add 1 to the counter
+# print the number of even numbers
