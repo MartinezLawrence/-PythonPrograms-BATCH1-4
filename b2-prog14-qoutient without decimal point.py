4a4ab6c410ac3c7ec95f975de5f03a5adcaed4a7
unknown <lawrencekulet@gmail.com>
2025-03-13 13:17:12 +0800

    prog 14 batch 2 code

--- b2-prog14-qoutient without decimal point.py
+++ b2-prog14-qoutient without decimal point.py
@@ -2,4 +2,8 @@
 # this program will display the quotient of two numbers without decimal point
 
 # prompt user to input two numbers
+num1 = float(input("Enter first number: ")) # first num
+num2 = float(input("Enter second number: ")) # second num
+
 # divide the two numbers and convert the result to integer
+print(int(num1 / num2)) # print the result
