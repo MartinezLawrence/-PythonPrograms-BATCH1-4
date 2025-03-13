e2b19114ac3f1ad487db53ac5bfbd16bb087a8fb
unknown <lawrencekulet@gmail.com>
2025-03-13 23:59:52 +0800

    pseudocode

---
+++ display_number_that_have_duplicate.py
@@ -0,0 +1,7 @@
+# display number that have duplicate
+# this program will display the number that have duplicate in the list
+
+# # Initialize an empty list to store user input numbers
+# loop 10 times to ask user to input number 10 times
+# use list comprehension to get the number that have duplicate
+# display the number that have duplicate
