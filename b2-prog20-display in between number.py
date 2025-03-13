d7dd5b230fa167518f1d32813abcebfc111c10c9
unknown <lawrencekulet@gmail.com>
2025-03-13 14:38:39 +0800

    prog 20 pseudocode

---
+++ b2-prog20-display in between number.py
@@ -0,0 +1,7 @@
+# display in between number
+# this program will print all the numbers between the two numbers given by the user
+
+# get the two numbers from the user
+# swap the numbers if the first number is greater than the second number
+# add 1 in range because the second number is not included in the range
+# print the numbers in between the two numbers
