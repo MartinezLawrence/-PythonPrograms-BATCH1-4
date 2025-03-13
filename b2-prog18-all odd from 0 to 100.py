844f08442e6da8a535a4ab5c0cafd0a552ed7695
unknown <lawrencekulet@gmail.com>
2025-03-13 14:23:30 +0800

    prog 18 batch 2 code

--- b2-prog18-all odd from 0 to 100.py
+++ b2-prog18-all odd from 0 to 100.py
@@ -1,7 +1,8 @@
 # all odd from 0 to 100
 # this program will print all odd numbers from 0 to 100 using while loop
 
-# initialize the variable
-# while loop will run until the value of number is less than 100
-# if the value of oddnum is not divisible by 2 then it will print the value of oddnum
-# increment the value of oddnum by 1
+oddnum = 0              # initialize the variable
+while oddnum <= 100:    # while loop will run until the value of number is less than 100
+    if oddnum % 2: 
+        print(oddnum)   # if the value of oddnum is not divisible by 2 then it will print the value of oddnum
+    oddnum += 1         # increment the value of oddnum by 1
