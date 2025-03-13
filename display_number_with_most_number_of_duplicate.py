a0fd7daa70550104bd71108f309c01c072770f37
unknown <lawrencekulet@gmail.com>
2025-03-14 00:12:18 +0800

    pseudocode

---
+++ display_number_with_most_number_of_duplicate.py
@@ -0,0 +1,9 @@
+# display number with most number of duplicate
+# This program will display the number with the most number of duplicates
+
+# Initialize the list
+# use a while loop to get the input from the user until invalid input is entered
+# attempt to convert the input to an integer
+# break out the loop if the input is invalid
+# find the maximum number of duplicates
+# use list comprehension to get the number with the most number of duplicates
