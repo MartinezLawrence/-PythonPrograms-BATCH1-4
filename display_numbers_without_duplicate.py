6ce3ffc94eb7fe5ba19fc313b7ce531756d9f666
unknown <lawrencekulet@gmail.com>
2025-03-13 22:14:05 +0800

    prog 21 halfway progress

--- display_numbers_without_duplicate.py
+++ display_numbers_without_duplicate.py
@@ -2,6 +2,11 @@
 # This program is to display numbers that has no duplicate
 
 # Initialize an empty list to store user input numbers
+numbers = []
+
 # Loop to ask the user for 10 numbers
+for list_of_numbers in range(10):   # 10 numbers
+    numbers.append(int(input("Enter a number: ")))  # Add the numbers to the list
+    
 # Use list comprehension to find numbers that appear only once
 # Print the numbers without duplicates
