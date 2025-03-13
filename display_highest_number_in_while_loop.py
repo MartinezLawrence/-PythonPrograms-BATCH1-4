aa6d63879d8f3324066cac265726238c197e03b4
unknown <lawrencekulet@gmail.com>
2025-03-14 00:23:37 +0800

    pseudocode

---
+++ display_highest_number_in_while_loop.py
@@ -0,0 +1,8 @@
+# display highest number in while loop
+# this program will display the highest number entered by the user
+
+# initialize the variables
+# use while loop to continously ask the user for a number
+# attempt to convert the input to a number
+# break out of the loop if the user enters an invalid input
+# print the maximum number entered by the user
