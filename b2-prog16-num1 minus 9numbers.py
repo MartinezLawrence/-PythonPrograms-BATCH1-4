25531a1515f7bee6a4e61fc27f6ca213ce9fe036
unknown <lawrencekulet@gmail.com>
2025-03-13 13:52:35 +0800

    prog 16 batch2 code

--- b2-prog16-num1 minus 9numbers.py
+++ b2-prog16-num1 minus 9numbers.py
@@ -2,6 +2,10 @@
 # this program will print the result of the first number minus all of the remaining numbers
 
 # prompt the user to enter the first number
-# the range of the loop is 9 because we are going to ask the user to enter 9 numbers
-# prompt the user to enter the next 9 number
+num1 = float(input("Enter the first number: "))
+
+for numbers in range(9):  # the range of the loop is 9 because we are going to ask the user to enter 9 numbers
+    num1 -= float(input("Enter your number: ")) # enter the next 9 number and subtract it from the first number
+
 # print the result of the first number minus all of the remaining numbers
+print(num1)
