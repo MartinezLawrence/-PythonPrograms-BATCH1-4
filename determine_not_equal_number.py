1557dae8684090d311e76a169c6c7c86c977892a
unknown <lawrencekulet@gmail.com>
2025-03-13 21:39:20 +0800

    renamed programs batch 2

---
+++ determine_not_equal_number.py
@@ -0,0 +1,10 @@
+# determine not equal number
+# this program will display if the numbers are not equal
+
+# prompt user to input the first number and second number
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# display "Not Equal" if the numbers are not equal
+if num1 != num2:  # compare the two numbers
+    print("Not Equal")  # print "Not Equal" if the numbers are not equal
