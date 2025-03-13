4dd59e417238c93b83c616d1f118d52c3f0fcce4
unknown <lawrencekulet@gmail.com>
2025-03-14 00:04:18 +0800

    prog26 finished

--- display_number_that_have_duplicate.py
+++ display_number_that_have_duplicate.py
@@ -2,6 +2,14 @@
 # this program will display the number that have duplicate in the list
 
 # # Initialize an empty list to store user input numbers
+numbers = []
+
 # loop 10 times to ask user to input number 10 times
+for list_of_numbers in range(10):   # 10 numbers
+    numbers.append(int(input("Enter a number: ")))  # Add the numbers to the list
+
 # use list comprehension to get the number that have duplicate
+duplicate = [n for n in numbers if numbers.count(n) > 1]
+
 # display the number that have duplicate
+print(duplicate)
