249c3bec7a0e6274e678f4f22a3c8e9be1a67271
unknown <lawrencekulet@gmail.com>
2025-03-13 12:31:18 +0800

    fixed prog1

--- b1-prog1 - bigger number finder.py
+++ b1-prog1 - bigger number finder.py
@@ -1,10 +1,10 @@
 # b1-prog1 - bigger num finder
 # Lawrence Andrew B. Martinez
 # This program will find the bigger number between two numbers
-# Prompt the user to input numb1 and num2
-# Compare the two numbers
-# Print the bigger number
 
+# Prompt the user to input num1 and num2
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+# Print the bigger number between num1 and num2
 print(max(num1, num2))
