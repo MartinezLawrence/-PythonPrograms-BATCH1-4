077a1848a46067ecd1855e31e24685585cfa7314
unknown <lawrencekulet@gmail.com>
2025-03-13 14:17:29 +0800

    prog 18 pseudocode

---
+++ b2-prog18-all odd from 0 to 100.py
@@ -0,0 +1,7 @@
+# all odd from 0 to 100
+# this program will print all odd numbers from 0 to 100 using while loop
+
+# initialize the variable
+# while loop will run until the value of number is less than 100
+# if the value of oddnum is not divisible by 2 then it will print the value of oddnum
+# increment the value of oddnum by 1
