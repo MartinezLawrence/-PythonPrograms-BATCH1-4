09b9928f12cb5a6b48dbae1863d9110bc106dc3c
unknown <lawrencekulet@gmail.com>
2025-03-14 00:35:51 +0800

    pseudocode

---
+++ display_average.py
@@ -0,0 +1,8 @@
+# display average
+# this program will display the average of the number inputted
+
+# initialize an empty list
+# use while loop to continuously get the input from the user
+# attempt to convert the input to an integer
+# break the loop if the input is invalid
+# calculate and print the average of the numbers in the list
