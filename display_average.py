3e98ac31171410d29b97035ba0a41ffe6e52148e
unknown <lawrencekulet@gmail.com>
2025-03-14 00:39:12 +0800

    prog30 finished

--- display_average.py
+++ display_average.py
@@ -2,7 +2,14 @@
 # this program will display the average of the number inputted
 
 # initialize an empty list
+number_list = []
+
 # use while loop to continuously get the input from the user
-# attempt to convert the input to an integer
-# break the loop if the input is invalid
+while True:
+    try:
+        number_list.append(int(input("Enter a number: ")))  # append the number to the list
+    except ValueError:
+        break  # break out the loop if the input is invalid
+
 # calculate and print the average of the numbers in the list
+print(sum(number_list) / len(number_list))  # sum of the list divided by the length of the list
